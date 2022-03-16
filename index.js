cad19b2325d29a515113c87bb4e71f971a5bd04e
Benjamin Ikeh <b.ikeh@yahoo.com>
2022-03-16 08:06:59 +0000

    add friend schema, email as array  of emails

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
-
+import { graphqlHTTP } from 'express-graphql';
+import schema from './schema';
 
 const app = express();
 
@@ -7,6 +8,23 @@ app.get('/', (req, res) => {
   res.send("GraphQl is amazing");
 });
 
+const root = { friend: ()=> {
+  return {
+    "id": 982093403,
+    "firstName": "Bridget",
+    "lastName": "Grace",
+    "gender": "Male",
+    "language": "English",
+    "email": [{ email: "grat@yahoo.com"}, {email: "soaddo@yahoo.com"}]
+  }
+}};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}));
 
 
-app.listen(8080, () => console.log("Running server on port 8080"));
+
+app.listen(8084, () => console.log("Running server on port 8084"));
